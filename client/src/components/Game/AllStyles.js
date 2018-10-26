62a6a7e28936e6d6705192c2dec8747074fe186c
03140420 <codeGeek.03.14@gmail.com>
2018-10-26 10:50:52 -0600

    moved the placeholder cards and replaced them with the card images we created.

--- client/src/components/Game/AllStyles.js
+++ client/src/components/Game/AllStyles.js
@@ -1,32 +1,35 @@
-const Card = theme => ( {
+const AllStyles = {};
 
-//card stack??
-    card: {
+const Card = theme => ( {
+	card: {
 		width: "10%",
 		marginTop: "1%",
 		marginBottom: "1%",
 		margin: ".1%",
 	},
-
-    //playerField
-	multicardDisplay: {
+	multicard_display: {
 		marginTop: "1%",
 		marginBottom: "1%",
 		width: "50%",
 		height: "40%",
 		margin: "1%"
 	},
-    //Gameboard page
 	page: {
 		width: "100%",
 		height: "100%",
-		[theme.breakpoints.between( "xs", "sm" )]: {
+		[
+			theme
+				.breakpoints
+				.between( 'xs', 'sm' )
+		]: {
 			paddingLeft: "14%",
 			paddingRight: "10%",
 			paddingTop: "1%",
-			background-image: url("../earth2.png"),
+			backgroundColor: "blue",
 		},
-		[ theme.breakpoints.only( "md" ) ]: {
+		[ theme
+				.breakpoints
+				.only( 'md' ) ]: {
 			maxWidth: "900px",
 			maxHeight: "800px",
 			paddingLeft: "14%",
@@ -34,7 +37,11 @@ const Card = theme => ( {
 			paddingTop: "1%",
 			backgroundColor: "green",
 		},
-		[theme.breakpoints.between( "lg", "xl" )]: {
+		[
+			theme
+				.breakpoints
+				.between( 'lg', 'xl' )
+		]: {
 			maxWidth: "1000px",
 			maxHeight: "750px",
 			paddingLeft: "14%",
@@ -42,8 +49,11 @@ const Card = theme => ( {
 			paddingTop: "1%",
 			backgroundColor: "red",
 		},
+		// height: "100%",
+		// maxWidth: "100%",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 	},
 } );
 export {
-	Card
+	AllStyles,
+	Card,
 }
